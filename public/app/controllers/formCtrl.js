9232fa30de1da3e6483491bb765970b1647a7e5b
eaglehunter18 <eaglehunter18@hotmail.com>
2017-03-20 16:19:58 +0200

    centering text in mobile layout + create css file for every grid

--- public/app/controllers/formCtrl.js
+++ public/app/controllers/formCtrl.js
@@ -1,15 +1,13 @@
 /**
  * Created by hhamra on 3/14/2017.
  */
-angular
-    .module('myApp').controller('formCtrl',function($scope,QueryService,$route)
-{
-
-
+ angular
+ .module('myApp').controller('formCtrl',function($scope,QueryService,$route)
+ {
 
 // get not statred bids  from server
-    QueryService.query('GET', 'api/v1/products/getUnstatrted', {})
-        .then(function(data) {
+QueryService.query('GET', 'api/v1/products/getUnstatrted', {})
+.then(function(data) {
             //console.log(data);
             $scope.myData=data.data;
         }, function(error) {
@@ -18,14 +16,17 @@ angular
 
 //push new bids to our table
 
-   $scope.bids=[];
-   $scope.myData=$scope.bids;
-    $("#datetimepicker1").datetimepicker({
-     format : "YYYY-MM-DD hh:mm:ss"
-    })
-    $scope.addRow = function(){
-        var dat = document.getElementById('startTime').value;
-        console.log(dat);
+$scope.bids=[];
+$scope.myData=$scope.bids;
+$("#datetimepicker1").datetimepicker({
+   format : "YYYY-MM-DD hh:mm:ss"
+});
+
+
+
+$scope.addRow = function(){
+    var dat = document.getElementById('startTime').value;
+    //console.log(dat);
         // dat1=new Date(dat);
         var newBid={
             'productName':$scope.productName,
@@ -38,9 +39,9 @@ angular
         };
         if($scope.checkboxmodel != true) {
             $scope.myData.push(newBid);
-            console.log('test');
+           // console.log('test');
           //  $route.reload();
-        }
+      }
         //console.log(newBid1);
         var newBid1={
             name:$scope.productName,
@@ -55,22 +56,23 @@ angular
 
 
         QueryService.query('POST', 'api/v1/products/addProduct', newBid1)
-            .then(function(data) {
-                console.log(data.data);
+        .then(function(data) {
+           // console.log(data.data);
                // $route.reload();
-            }, function(error) {
-                console.log(error);
-            });
+           }, function(error) {
+            console.log(error);
+        });
 
 
 //empty scope for next  bid
-        $scope.productName='';
-        $scope.productDesc='';
-        $scope.period='';
-        $scope.incUnit='';
+$scope.productName='';
+$scope.productDesc='';
+$scope.period='';
+$scope.incUnit='';
+$scope.price='';
 
-        $('#myModal').modal('hide');
-    };
+$('#myModal').modal('hide');
+};
 
 $scope.changeToStarted=function (index) {
 
@@ -78,22 +80,12 @@ $scope.changeToStarted=function (index) {
         id:$scope.myData[index].id,
     };
 
-
     QueryService.query('POST', 'api/v1/products/changeToStarted',bidId )
-        .then(function(data) {
-            console.log(data);
-            $route.reload();
-        }, function(error) {
-            console.log(error);
-        });
-
-
+    .then(function(data) {
+        //console.log(data);
+        $route.reload();
+    }, function(error) {
+        console.log(error);
+    });
 }
-
-    // console.log("testttttttttttttttt");
-    // $('#submit').click(function() {
-    //
-    //     $('#myModal').modal('hide');
-    // });
-
 });
